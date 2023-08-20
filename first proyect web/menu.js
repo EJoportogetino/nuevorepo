548b01b9d007609ba22f69585608e72a0bf3ad9c
EJoportogetino <julian.oporto.251@gmail.com>
2023-08-20 13:09:55 -0300

    hola aliens

---
+++ first proyect web/menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function(){
+  var menuElement = document.getElementById("menu");
+  menuElement.innerHTML= `
+<link rel='stylesheet' href="./onda.css" >
+<ul>
+ <li><a href="encabezado.html">Pagina principal</a></li>
+ <li><a href="proyectos.html">Proyects</a></li>
+ <li><a href="cv.html">CV</a></li>
+ <li><a href="fotos.html">Photos</a></li>
+ <li><a href="poesia.html">Poetry</a></li>
+ <li><a href="aboutme.html">Sobre mi</a></li>
+
+</ul>
+`;
+});
---
+++ first proyect web/pie.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+  var pieElement = document.getElementById("pie");
+  pieElement.innerHTML= `
+<ul>
+<p>si llegaste hasta aca contratame plis contacto:julian.oporto.251(gmail)</p>
+</ul>
+`;
+});
