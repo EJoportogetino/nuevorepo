b3e38ba0a18edf110697a7e8cae0f8bd34e7ae1a
EJoportogetino <julian.oporto.251@gmail.com>
2023-09-03 11:54:29 -0300

    arreglo problema carpeta

---
+++ aliens/menu.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded",function(){
+  var menuElement = document.getElementById("menu");
+  menuElement.innerHTML= `
+<!--<link rel='stylesheet' href="./estilo.css" > -->
+<table>
+  <tbody>
+   <tr>
+    <td><a href="PP">Pagina principal</td>
+    <td><a href="Equipos">Equipos</td>
+    <td><a href="Accesorios">Accesorios</td>
+    <td><a href="Liquidos">Liquidos</td>
+    <!-- <td><a href="Contacto">Contacto</td> -->
+   </tr>
+  </tbody>
+</table>
+`;
+});
---
+++ aliens/pie.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+  var pieElement = document.getElementById("pie");
+  pieElement.innerHTML= `
+  <div class= "hyper">
+      <a target="_blank" href="https://wa.me/5492915278302">Nuestro WhatsApp</a>
+      <a target="_blank" href="https://www.instagram.com/alienseliquids">Nuestro Instagram</a>
+      <a target="_blank" href="https://www.facebook.com/alienseliquids">Nuestro Facebook</a>
+  </div>
+`;
+});
